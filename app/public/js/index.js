a52ad42084223eeb97063c0d601ad28198f68d1b
varmacost <89751284+varmacost@users.noreply.github.com>
2021-09-22 16:37:51 -0400

    post hw 3 bugs push

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -1,48 +1,41 @@
-const Offer = {
-    data() {
-        return {
-            foo: 0
-            msg: "D&S is my favorite",
-            "offers":[
-                {"id": 1,
-                "name":"Vivi Armacost",
-                "salary": 100000,
-                "bonus": 5000, 
-                "company": "EY",
-                "offerDate": "2021-08-01"
-                },
-                {"id": 2,
-                "name":"Will Austin",
-                "salary": 90000,
-                "bonus": 10000, 
-                "company": "Deloitte",
-                "offerDate": "2021-08-19"
-                }
-            ]
-        }
-    }
-}
-async mounted() {
+const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 
-    fetch('https://randomuser.me/api/', {
-    })
-    .then(response => response.json())
-    .then((responseJson) => {
-        console.log(responseJson);
-        this.result = responseJson.results[0];
-        this.message = this.result.name;
-    }
-    .catch( (error) => {
-        this.message = error;
-        console.error(error);
-    });
-    {
-        data() { /*...*/ },
-        computed: {}, // <-- NEW
-        methods: {}, // <-- NEW
-        created() { /*...*/ }
-      }
+const comparer = (idx, asc) => (a, b) => ((v1, v2) =>
+    v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 
-    // Source: https://github.com/tag/msis-lesson-plan-2020/blob/master/06%20HTTP%20and%20VueJS%20II.md
+// do the work...
+document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
+    const table = th.closest('table');
+    Array.from(table.querySelectorAll('tr:nth-child(n+2)'))
+        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+        .forEach(tr => table.appendChild(tr) );
+})));
 
-}
+
+// Source: file:///Users/a/Documents/GitHub/2-using-vue-js-varmacos/app/public/books.html#
+// const Offer = {
+//     data() {
+//         return {
+//             foo: 0
+//             msg: "D&S is my favorite",
+//             "offers":[
+//                 {"id": 1,
+//                 "name":"Vivi Armacost",
+//                 "salary": 100000,
+//                 "bonus": 5000,
+//                 "company": "EY",
+//                 "offerDate": "2021-08-01"
+//                 },
+//                 {"id": 2,
+//                 "name":"Will Austin",
+//                 "salary": 90000,
+//                 "bonus": 10000,
+//                 "company": "Deloitte",
+//                 "offerDate": "2021-08-19"
+//                 }
+//             ]
+//         }
+//     }
+// }
+// Vue.createApp(Counter).mount('#classtest')
